21576bf107be90f1f9f2479751be86365d53c9d4
HeitorPereiradaSilva11Ti-A <161168386+HeitorPereiradaSilva11Ti-A@users.noreply.github.com>
2025-11-09 18:45:24 -0300

    parte 2 do  trabalho feita

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,18 @@ const app = express()
 const port = 3333
 
 app.use(cors())
-
-
+app.use(express.json())
 // GET, POST, PATCH, PUT, DELETE
 app.get("/", (request, response) => {
     response.json(persons)
+})
+
+app.post("/cadastrar", (request, response) => {
+    const {name, email, age, nickname, password} =  request.body.user
+
+    console.log(name, email, age, nickname, password)
+
+    response.status(201).json({message: "Usuário cadastrado com sucesso!"})
 })
 
 // 3000, 3001, 3002, 3003, 3333, 5000, 5555, 5500, 8080
